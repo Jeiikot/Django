83814dfc9e390ef7a5024fc72316bafecb1d5965
Jeiikot <jeiikot21@gmail.com>
2020-09-07 00:18:52 -0500

    [Add] views-urls-models files

--- dj4e/mysite/cats/urls.py
+++ dj4e/mysite/cats/urls.py
@@ -6,13 +6,14 @@ from . import views
 app_name='cats'
 urlpatterns = [
     path('', views.MainView.as_view(), name='all'),
-    path('main/create/', views.AutoCreate.as_view(), name='auto_create'),
-    path('main/<int:pk>/update/', views.AutoUpdate.as_view(), name='auto_update'),
-    path('main/<int:pk>/delete/', views.AutoDelete.as_view(), name='auto_delete'),
-    path('lookup/', views.MakeView.as_view(), name='make_list'),
-    path('lookup/create/', views.MakeCreate.as_view(), name='make_create'),
-    path('lookup/<int:pk>/update/', views.MakeUpdate.as_view(), name='make_update'),
-    path('lookup/<int:pk>/delete/', views.MakeDelete.as_view(), name='make_delete'),
+    path('main/create/', views.CatCreate.as_view(), name='cat_create'),
+    path('main/<int:pk>/update/', views.CatUpdate.as_view(), name='cat_update'),
+    path('main/<int:pk>/delete/', views.CatDelete.as_view(), name='cat_delete'),
+
+    path('lookup/', views.BreedView.as_view(), name='breed_list'),
+    path('lookup/create/', views.BreedCreate.as_view(), name='breed_create'),
+    path('lookup/<int:pk>/update/', views.BreedUpdate.as_view(), name='breed_update'),
+    path('lookup/<int:pk>/delete/', views.BreedDelete.as_view(), name='breed_delete'),
 ]
 
 # Note that make_ and auto_ give us uniqueness within this application
