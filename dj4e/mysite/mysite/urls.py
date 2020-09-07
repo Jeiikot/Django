f79bea3b3ab6269a8ee1f69bd036392307247748
Jeiikot <jeiikot21@gmail.com>
2020-09-06 23:09:48 -0500

    [Add] Main Page

--- dj4e/mysite/mysite/urls.py
+++ dj4e/mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home/main.html')),
 ]
