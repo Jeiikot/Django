9bdf40b0738b4f60126e68092b5f75ea22c68da1
Jeiikot <jeiikot21@gmail.com>
2020-09-06 22:24:35 -0500

    [End] C2-Week2

--- dj4e/mysite/polls/views.py
+++ dj4e/mysite/polls/views.py
@@ -1,18 +1,17 @@
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -20,3 +19,6 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def owner(request):
+    return HttpResponse("Hello, world. c9238300 is the polls index.")
