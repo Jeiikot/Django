31a822b21230762ff64af3c9fecb13db4cb053c5
Jeiikot <jeiikot21@gmail.com>
2020-09-13 18:50:54 -0500

    [Add] forms.py and humanize.py

--- dj4e/mysite/ads/models.py
+++ dj4e/mysite/ads/models.py
@@ -10,6 +10,10 @@ class Ad(models.Model) :
     )
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
     text = models.TextField()
+
+    picture = models.BinaryField(null=True, editable=True)
+    content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
+
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
