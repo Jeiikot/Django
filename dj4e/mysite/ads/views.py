645430e2de33cc685767cafef76003435cd12d16
Jeiikot <jeiikot21@gmail.com>
2020-09-15 00:23:26 -0500

    [Add] code in views.py

--- dj4e/mysite/ads/views.py
+++ dj4e/mysite/ads/views.py
@@ -139,5 +139,34 @@ class DeleteFavoriteView(LoginRequiredMixin, View):
 
         return HttpResponse()
 
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from ads.utils import dump_queries
 from django.db.models import Q
+
+class PostListView(View):
+    template_name = "well/list.html"
+
+    def get(self, request):
+        strval = request.GET.get("search", False)
+        if strval:
+            # Simple title-only search
+            # objects = Post.objects.filter(title__contains=strval).select_related().order_by('-updated_at')[:10]
+
+            # Multi-field search
+            query = Q(title__contains=strval)
+            query.add(Q(text__contains=strval), Q.OR)
+            objects = Ad.objects.filter(query).select_related().order_by('-updated_at')[:10]
+        else :
+            # try both versions with > 4 posts and watch the queries that happen
+            objects = Ad.objects.all().order_by('-updated_at')[:10]
+            # objects = Post.objects.select_related().all().order_by('-updated_at')[:10]
+
+        # Augment the post_list
+        for obj in objects:
+            obj.natural_updated = naturaltime(obj.updated_at)
+
+        ctx = {'post_list': objects, 'search': strval}
+        retval = render(request, self.template_name, ctx)
+
+        dump_queries()
+        return retval
